9cd8c813d5f856d214c6d36c2c30783b84fa4a4d
yayano <boussetta13yahia@gmail.com>
2021-05-25 11:31:15 +0100

    deploy

--- app.js
+++ app.js
@@ -43,11 +43,11 @@ app.use('/contact', (req, res) => {
     }
   });
 });
-if (process.env.NODE_ENV === 'developpement' || true) {
+if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname + '/client/build')));
-  // app.get('*', (req, res) => {
-  //   res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-  // });
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 app.listen(port, () => {
   console.log(`listening to port :${port}`);
